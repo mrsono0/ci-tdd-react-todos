200ba1c9633422d5ea9543d049d8ae3233a8f143
Boyaa <cbh0548@icloud.com>
2022-10-18 11:32:21 +0900

    TodoApp TodoForm rendering test oK

--- src/TodoApp_Boyaa.test.js
+++ src/TodoApp_Boyaa.test.js
@@ -8,4 +8,9 @@ describe('<TodoApp_Boyaa />', () => {
         getByText('등록'); // TodoForm이 화면에 있는지 확인 
         getByTestId('TodoList'); // TodoList 화면에 있는지 확인 
     });
+    it('renders two defaults todos', () => {
+        const {getByText} = render(<TodoApp_Boyaa />);
+        getByText('TDD 배우기');
+        getByText('react-testing-library');
+    });
 });
